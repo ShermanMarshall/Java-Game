4bf5a2afee56bb78e68aeadc810ac51c7d0ec13d
ShermanMarshall <sherman.david.marshall@gmail.com>
2015-07-07 11:48:36 -0700

    Update Plane.java

--- CS493Project/src/cs493project/Plane.java
+++ CS493Project/src/cs493project/Plane.java
@@ -4,7 +4,6 @@ import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
